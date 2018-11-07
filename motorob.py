39229425ebb86d5248d028a062a63a16b07797cf
Odd Andre <oddandreowren@gmail.com>
2018-11-07 15:22:32 +0100

    Motorob ferdig

--- motorob.py
+++ motorob.py
@@ -2,46 +2,31 @@ from motors import Motors
 from sensob import CameraSensob
 from time import sleep
 
-class Motob:
-    """
 
-    """
+class Motob:
+
     def __init__(self, bbcon):
         self.bbcon = bbcon
-        # Most recent motor recommendation sent to the motob
         self.values = []
-        # Objektet Motors
         self.motor = Motors()
-        self.speedDic = {100: 1, 75: 0.75, 50: 0.5, 40: 0.4, 35: 0.35, 33: 0.33, 30: 0.3, 25: 0.25, 20: 0.2, 10: 0.1, 5: 0.05, 0: 0.0}
-        self.can_take_photo = False
+        self.photograph = False
+        self.camera=CameraSensob()
 
     def update(self, motor_recommandation):
-        """
-        receive a new motor recommendation, load it into the value slot, and operationalize it.
-        :param motorvalue:
-        :return: Nothing
-        """
+        # Mottar en anbefaling fra bbcon og behaviors
+
         self.values = motor_recommandation
         self.operationlize()
 
     def operationlize(self):
-        """
-        L - Left
-        R - Right
-        F - Forwards
-        S - Stop
-        B - Backwards
-        FL - Turning left while driving forward
-        FR - Turning right while driving forward
-        The second vector elemnt is the degree og turning.
-        Exp: rec
-        :return:
-        """
+        # Henter ut første verdi fra anbefalinger, antall grader gis som andre vektor i self.values dersom anbefaling er
+        # 'l' eller 'r'
+
         value=self.values[0]
-        print("Motor Req = ", value)
+        print("Motor Recommandation = ", value)
         if value == "f":
             print("Forward")
-            self.motor.set_value([self.speedDic[50], self.speedDic[50]],0.15)
+            self.motor.set_value([0.5, 0.5],0.15)
         elif value == "l":
             print("Left")
             self.motor.set_value([-1,1], self.turn_n_degrees(self.values[1]))
@@ -50,29 +35,24 @@ class Motob:
             self.motor.set_value([1,-1], self.turn_n_degrees(self.values[1]))
         elif value == 'fl':
             print('Left and forward')
-            self.motor.set_value([self.speedDic[5], self.speedDic[35]],0.15)
+            self.motor.set_value([0.05, 0.35],0.15)
         elif value == 'fr':
             print('Right and forward')
-            self.motor.set_value([self.speedDic[35], self.speedDic[5]],0.15)
+            self.motor.set_value([0.35, 0.05],0.15)
         elif value == 't':
-            self.motor.set_value([-1, 1], 0.5)
-            self.motor.set_value([1, -1], 0.5)
-            print("Doing some sick tricks! #hardcore")
+            self.motor.set_value([-0.5, 0.5], 0.25)
+            self.motor.set_value([0.5, -0.5], 0.25)
+            print("Found red!")
             self.bbcon.photo_taken()
         elif value == "s":
             print("Stop")
             self.motor.stop()
-            self.can_take_photo = True
+            self.photograph = True
             sleep(1)
         elif value == 'p':
-            print('CHEEEEESE!')
-            CameraSensob().update()
+            self.camera.update()
 
     @staticmethod
     def turn_n_degrees(deg):
-        """
-        Takes in the desired turn degree and returns how long the motors have to turn at full speed.
-        :param deg: Desired turn degree.
-        :return: Time.
-        """
+        # Returnerer antall sekunder motorene må kjøres på full speed, henholdsvis frem og bak for å tilsvare grader
         return 0.0028 * deg
