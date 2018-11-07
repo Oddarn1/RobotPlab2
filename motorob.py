71a0898c5df109b0dd4757e68007561497af76f0
Odd André Owren <oddandreowren@gmail.com>
2018-11-07 15:06:07 +0100

    Update motorob.py

--- motorob.py
+++ motorob.py
@@ -37,43 +37,36 @@ class Motob:
         Exp: rec
         :return:
         """
-
-        for value in self.values:
-            print("Motor Req = ", value)
-            if value == "f":
-                print("Forward")
-                self.motor.set_value([self.speedDic[25], self.speedDic[25]],0.25)
-            elif value == "l":
-                print("Left")
-                self.motor.set_value([-1 * self.speedDic[25], self.speedDic[30]], 0.25)
-            elif value == "r":
-                print("Right")
-                self.motor.set_value([self.speedDic[30], -1 * self.speedDic[25]], 0.25)
-            elif value == 'fl':
-                print('Left and forward')
-                self.motor.set_value([self.speedDic[5], self.speedDic[35]])
-            elif value == 'fr':
-                print('Right and forward')
-                self.motor.set_value([self.speedDic[35], self.speedDic[5]])
-            elif value == 't':
-                self.motor.set_value([-1, 1], 0.5)
-                self.motor.set_value([1, -1], 0.5)
-                print("Doing some sick tricks! #hardcore")
-                self.bbcon.photo_taken()
-            elif value == "s":
-                print("Stop")
-                self.motor.stop()
-            elif value == "b":
-                print("Backwards")
-                self.motor.backward(0.9, 0.50)
-                self.can_take_photo = True
-                sleep(1)
-            elif value == 'p':
-                print('CHEEEEESE!')
-                CameraSensob().update()
-            elif value == "none":
-                continue
-
+        value=self.values[0]
+        print("Motor Req = ", value)
+        if value == "f":
+            print("Forward")
+            self.motor.set_value([self.speedDic[50], self.speedDic[50]],0.15)
+        elif value == "l":
+            print("Left")
+            self.motor.set_value([-1,1], self.turn_n_degrees(self.values[1]))
+        elif value == "r":
+            print("Right")
+            self.motor.set_value([1,-1], self.turn_n_degrees(self.values[1]))
+        elif value == 'fl':
+            print('Left and forward')
+            self.motor.set_value([self.speedDic[5], self.speedDic[35]],0.15)
+        elif value == 'fr':
+            print('Right and forward')
+            self.motor.set_value([self.speedDic[35], self.speedDic[5]],0.15)
+        elif value == 't':
+            self.motor.set_value([-1, 1], 0.5)
+            self.motor.set_value([1, -1], 0.5)
+            print("Doing some sick tricks! #hardcore")
+            self.bbcon.photo_taken()
+        elif value == "s":
+            print("Stop")
+            self.motor.stop()
+            self.can_take_photo = True
+            sleep(1)
+        elif value == 'p':
+            print('CHEEEEESE!')
+            CameraSensob().update()
 
     @staticmethod
     def turn_n_degrees(deg):
